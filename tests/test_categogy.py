bac8b8c41b13f019630f3a8c37f5093f88e4d84e
WingBasil <i@vyarygin.ru>
2025-01-15 19:37:19 +0700

    Less_14_1

---
+++ tests/test_categogy.py
@@ -0,0 +1,10 @@
+def test_category(first_category, second_category):
+    assert first_category.name == "Category"
+    assert first_category.description == "Description of the category"
+    assert len(first_category.products) == 2
+
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+
+    assert first_category.product_count == 5
+    assert second_category.product_count == 5
