e68f8d58fb64c2866e53e2edd0f7bbd10ad48956
WingBasil <i@vyarygin.ru>
2025-01-20 19:32:49 +0700

    + homework_14_2

--- tests/test_categogy.py
+++ tests/test_categogy.py
@@ -1,10 +1,27 @@
-def test_category(first_category, second_category):
-    assert first_category.name == "Category"
-    assert first_category.description == "Description of the category"
-    assert len(first_category.products) == 2
+from src.product import Product
 
-    assert first_category.category_count == 2
-    assert second_category.category_count == 2
+def test_product(first_product, second_product):
+    assert first_product.name == "Product"
+    assert first_product.description == "Description of the product"
+    assert first_product.price == 84.50
+    assert first_product.quantity == 10
 
-    assert first_category.product_count == 5
-    assert second_category.product_count == 5
+    assert second_product.name == "Product number two"
+    assert second_product.description == "Description of the product number two"
+    assert second_product.price == 155.87
+    assert second_product.quantity == 34
+
+def test_new_product(product_dict):
+    product4 = Product.new_product(product_dict)
+    assert product4.name == "Product 4"
+    assert product4.description == "Description of the product 4"
+    assert product4.price == 145.75
+    assert product4.quantity == 23
+
+def test_prod_price_property(capsys, first_product):
+    first_product.price = -756.57
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или орицательная"
+    first_product.price = 756.57
+    assert first_product.price == 756.57
+
